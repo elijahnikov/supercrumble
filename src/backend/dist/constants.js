67bc290bc8bc44994b3b553ed6c08ac5bb5057e9
Elijah Posnikov <elijah@Elijahs-MBP.cust.communityfibre.co.uk>
2022-08-13 23:13:16 +0100

    removing dist

--- src/backend/dist/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FORGOT_PASS = exports.COOKIE_NAME = exports.prod = void 0;
-exports.prod = process.env.NODE_ENV === 'production';
-exports.COOKIE_NAME = 'cid';
-exports.FORGOT_PASS = 'forget-password';
-//# sourceMappingURL=constants.js.map
