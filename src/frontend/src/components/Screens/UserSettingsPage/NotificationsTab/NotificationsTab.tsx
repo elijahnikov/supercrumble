623465a7472b825d291890196dde718f89c4a412
elijahnikov <posnikovelijah@gmail.com>
2022-12-27 21:47:00 +0000

    work starting on notifications tab in settings page

--- src/frontend/src/components/Screens/UserSettingsPage/NotificationsTab/NotificationsTab.tsx
+++ src/frontend/src/components/Screens/UserSettingsPage/NotificationsTab/NotificationsTab.tsx
@@ -2,8 +2,14 @@ interface NotificationsTabProps {}
 
 const NotificationsTab = ({}: NotificationsTabProps) => {
     return (
-        <div>
-            <h1>Notifications Tab</h1>
+        <div className='p-5'>
+            <div className='float-left text-left'>
+                <h3>Notifications</h3>
+                <p className='text-sm text-superRed'>
+                    Choose when and how you recieve emails and/or in-app
+                    notifications from us.
+                </p>
+            </div>
         </div>
     );
 };
